07998969031dd40146e0cada6f4e905342a50f64
emong <a34451357@gmail.com>
2026-05-19 12:40:14 +0900

    fix: move docs into docs/ folder, update push.bat

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
-echo [Team Docs] 변경사항 확인중...
+echo [Syn Docs] 변경사항 확인중...
 git status --short
 echo.
 git add .
@@ -8,5 +8,5 @@ git commit -m "docs: update documents"
 git push
 echo.
 echo [완료] 사이트에 반영까지 1-2분 소요됩니다.
-echo https://MaoEmong.github.io
+echo https://maoemong.github.io/syn-docs
 pause
